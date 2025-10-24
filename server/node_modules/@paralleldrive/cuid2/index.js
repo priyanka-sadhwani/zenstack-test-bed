c29d41ccc4ba5b68502de965dfd8c3effb75e194
Priyanka Sadhwani <priyanka.sadhwani@hotmail.com>
2025-10-24 19:06:44 +0800

    remove node_modules

--- server/node_modules/@paralleldrive/cuid2/index.js
+++
@@ -1,6 +0,0 @@
-import { createId, init, getConstants, isCuid } from "./src/index.js";
-
-export { createId };
-export { init };
-export { getConstants };
-export { isCuid };
--- server/node_modules/error-causes/src/index-test.js
+++
@@ -1,4 +0,0 @@
-/* global require, module */
-// eslint-disable-next-line no-global-assign
-require = require("esm")(module);
-require("./test.js");
